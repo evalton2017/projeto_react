4799f4162082b76d2e33a14bfbc436fdc37ade96
Evalton Gomes <36282520+evalton2017@users.noreply.github.com>
2020-02-27 15:57:37 -0300

    criacao crud

--- src/projeto/projeto.jsx
+++ src/projeto/projeto.jsx
@@ -1,15 +1,54 @@
 import React,{Component} from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import ProjetoForm from './projetoForm'
 import ProjetoList from './projetoList'
 
+const URL = 'http://localhost:3003/api/';
+
 export default class Projeto extends Component{
+
+    constructor(props){
+        super(props)
+        this.state= {description: '', list: []}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove=this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp=>this.setState({...this.state, description: '', list:resp.data}))
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd(){
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(projeto){
+       axios.delete(`${URL}/${projeto.__id}`)
+         .then(resp=>this.refresh())
+    }
+
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <ProjetoForm></ProjetoForm>
-                <ProjetoList></ProjetoList>
+                <ProjetoForm description={this.state.description}
+                 handleChange={this.handleChange}
+                 handleAdd={this.handleAdd}/>
+                <ProjetoList  list={this.state.list}
+                    handleRemove={this.handleRemove}
+                />
             </div>
         )
     }
--- src/projeto/projetoList.jsx
+++ src/projeto/projetoList.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props=>(
-    <div>
-        <h2>Lista</h2>
-    </div>
-)
+export default props=>{
+    const renderRows = () =>{
+        const list = props.list || []
+        return list.map(projeto=>(
+            <tr key={projeto._id}>
+                <td>{projeto.description}</td>
+                <td>
+                    <IconButton style='danger' icon='trash-o' onClick={()=>props.handleRemove(projeto)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
